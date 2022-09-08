d1a0eb1032721d30faeec984302e97a7f5e4d896
Aleksi Backman <bddubbb@gmail.com>
2022-09-08 22:04:29 +0300

    Add script for downloading assets

---
+++ scripts/download_assets.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+pushd ..
+
+if [ "rogue_forever" != "$(basename $(pwd))" ] ; then
+	echo "Unexpected current directory. Please cd to scripts directory and run ./download_assets.sh"
+	exit 0
+fi
+
+if [ ! -d assets ] ; then
+	mkdir assets
+fi
+
+pushd assets
+
+wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/crawl-tiles/crawl-tiles%20Oct-5-2010.zip
+unzip -d ./ 'crawl-tiles Oct-5-2010.zip'
+mv 'crawl-tiles Oct-5-2010'/* ./
+rm 'crawl-tiles Oct-5-2010.zip'
+rmdir 'crawl-tiles Oct-5-2010'
+
+popd
+popd
