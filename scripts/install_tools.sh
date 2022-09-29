9e390f3229581de824dbc484b45eb1c7b399d080
Aleksi Backman <bddubbb@gmail.com>
2022-09-29 06:11:42 +0300

    CI: asset related installations

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -9,3 +9,4 @@ echo -e "Installing Texturepacker"
 echo -e "========================"
 wget https://www.codeandweb.com/download/texturepacker/6.0.2/TexturePacker-6.0.2.deb
 sudo dpkg -i TexturePacker-6.0.2.deb
+rm TexturePacker-6.0.2.deb
