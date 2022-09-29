4b6b9307ade81f8153054f2a826a1e780083e2f2
Aleksi Backman <bddubbb@gmail.com>
2022-09-29 06:46:35 +0300

    CI: test finds to not fail

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -13,12 +13,12 @@ sudo apt-get -f install # Install deps
 
 # TODO remove when done debugging
 echo Find 1
-find / -name libEGL.so.1 2> /dev/null
+find / -name libEGL.so.1 2> /dev/null || true
 
 sudo apt-get install libegl1 # Seems to be missing even after fixing deps above
 
 # TODO remove when done debugging
 echo Find 2
-find / -name libEGL.so.1 2> /dev/null
+find / -name libEGL.so.1 2> /dev/null || true
 
 rm TexturePacker-6.0.2.deb
