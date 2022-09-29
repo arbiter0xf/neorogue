459eca0f68fd7608017462592d6068fa0fc94905
Aleksi Backman <bddubbb@gmail.com>
2022-09-29 07:14:00 +0300

    CI: Remove debug code

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -10,15 +10,6 @@ echo -e "========================"
 wget https://www.codeandweb.com/download/texturepacker/6.0.2/TexturePacker-6.0.2.deb
 sudo dpkg -i TexturePacker-6.0.2.deb || true # There may be uninstalled deps
 sudo apt-get -f install # Install deps
-
-# TODO remove when done debugging
-echo Find 1
-find / -name libEGL.so.1 2> /dev/null || true
-
 sudo apt-get install libegl1 # Seems to be missing even after fixing deps above
-
-# TODO remove when done debugging
-echo Find 2
-find / -name libEGL.so.1 2> /dev/null || true
 
 rm TexturePacker-6.0.2.deb
