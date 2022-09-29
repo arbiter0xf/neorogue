93e9e77f692cb02ce860cb6e1f78c0f6c7750b97
Aleksi Backman <bddubbb@gmail.com>
2022-09-29 06:21:33 +0300

    CI: Handle missing texturepacker deps

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -8,5 +8,6 @@ echo -e "========================"
 echo -e "Installing Texturepacker"
 echo -e "========================"
 wget https://www.codeandweb.com/download/texturepacker/6.0.2/TexturePacker-6.0.2.deb
-sudo dpkg -i TexturePacker-6.0.2.deb
+sudo dpkg -i TexturePacker-6.0.2.deb || true # There may be uninstalled deps
+sudo apt-get -f install # Install deps
 rm TexturePacker-6.0.2.deb
