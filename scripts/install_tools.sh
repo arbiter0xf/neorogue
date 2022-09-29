5543294d255efd2980a5b6cb3bc5432fe746a421
Aleksi Backman <bddubbb@gmail.com>
2022-09-29 06:38:04 +0300

    CI: Try to install missing Texturepacker dependencies

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -10,4 +10,15 @@ echo -e "========================"
 wget https://www.codeandweb.com/download/texturepacker/6.0.2/TexturePacker-6.0.2.deb
 sudo dpkg -i TexturePacker-6.0.2.deb || true # There may be uninstalled deps
 sudo apt-get -f install # Install deps
+
+# TODO remove when done debugging
+echo Find 1
+find / -name libEGL.so.1 2> /dev/null
+
+sudo apt-get install libegl1 # Seems to be missing even after fixing deps above
+
+# TODO remove when done debugging
+echo Find 2
+find / -name libEGL.so.1 2> /dev/null
+
 rm TexturePacker-6.0.2.deb
