62c5cea397ee8b68ff34185af7ee1a56fa330bdc
Aleksi Backman <bddubbb@gmail.com>
2022-09-30 09:44:11 +0300

    Add timestamps to logs

--- src/Log.cpp
+++ src/Log.cpp
@@ -1,21 +1,36 @@
 #include "Log.hpp"
 
+#include <chrono>
+
 void Log::w(std::string msg)
 {
-    std::cout << WARN << msg << std::endl;
+    std::string warn = WARN + msg;
+    Log::log(warn);
 }
 
 void Log::i(std::string msg)
 {
-    std::cout << INFO << msg << std::endl;
+    std::string info = INFO + msg;
+    Log::log(info);
 }
 
 void Log::e(std::string msg)
 {
-    std::cout << ERR << msg << std::endl;
+    std::string error = ERR + msg;
+    Log::log(error);
 }
 
 void Log::d(std::string msg)
 {
-    std::cout << DBG << msg << std::endl;
+    std::string debug = DBG + msg;
+    Log::log(debug);
+}
+
+void Log::log(std::string msg)
+{
+    auto currentTime = std::chrono::system_clock::now();
+    auto currentTimeMillis =
+        std::chrono::duration_cast<std::chrono::milliseconds>(currentTime.time_since_epoch());
+
+    std::cout << currentTimeMillis.count() << " " << msg << std::endl;
 }
