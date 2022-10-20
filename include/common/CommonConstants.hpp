8f50449d8e8c193167e733a5e73c688f328d4eef
Aleksi Backman <bddubbb@gmail.com>
2022-10-20 22:36:32 +0300

    Move game headers under include/game and add include/common

---
+++ include/common/CommonConstants.hpp
@@ -0,0 +1,8 @@
+#ifndef COMMON_CONSTANTS_HPP_DEFINED
+#define COMMON_CONSTANTS_HPP_DEFINED
+
+namespace g_common_constants {
+    inline constexpr int PLACEHOLDER = -1;
+}
+
+#endif // COMMON_CONSTANTS_HPP_DEFINED
