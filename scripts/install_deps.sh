e26f6c96e69df49894ea8f5e816f7d3893a9c667
Aleksi Backman <arbiter0xf@protonmail.com>
2024-07-07 12:37:12 +0300

    Add main function to deps install script

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -8,61 +8,65 @@ readonly INSTALL_GTEST="TRUE"
 readonly INSTALL_CMAKE="TRUE"
 readonly INSTALL_CPP_COMPILER="TRUE"
 
+main() {
+    if [ ! -d deps_install_workarea ] ; then
+        mkdir deps_install_workarea
+    fi
+
+    pushd deps_install_workarea
+
+    if [ "TRUE" == "${INSTALL_CMAKE}" ] ; then
+        sudo apt update
+        sudo apt install cmake
+    fi
+
+    if [ "TRUE" == "${INSTALL_CPP_COMPILER}" ] ; then
+        sudo apt update
+        sudo apt install g++
+    fi
+
+    if [ "TRUE" == "$INSTALL_SDL" ] ; then
+        sudo apt install libx11-dev libxext-dev
+
+        wget https://github.com/libsdl-org/SDL/archive/refs/tags/release-2.24.0.zip
+        unzip release-2.24.0.zip
+        pushd SDL-release-2.24.0
+        ./configure
+        make
+        sudo make install
+        popd
+    fi
+
+    if [ "TRUE" == "$INSTALL_SDL_IMAGE" ] ; then
+        sudo apt install libx11-dev libxext-dev
+
+        wget https://github.com/libsdl-org/SDL_image/archive/refs/tags/release-2.6.2.zip
+        unzip release-2.6.2.zip
+        pushd SDL_image-release-2.6.2
+        ./configure
+        make
+        sudo make install
+        popd
+    fi
+
+    popd # deps_install_workarea
+
+    pushd ../
+    if [ ! -d external ] ; then
+        mkdir external
+    fi
+
+    popd # ../
+
+    pushd ./deps_install_workarea
+    if [ "TRUE" == "${INSTALL_GTEST}" ] ; then
+        ../install_gtest.sh
+    fi
+    popd # ./deps_install_workarea
+
+    sudo rm -rf ./deps_install_workarea
+}
+
 cd $(dirname $0)
 
-if [ ! -d deps_install_workarea ] ; then
-    mkdir deps_install_workarea
-fi
-
-pushd deps_install_workarea
-
-if [ "TRUE" == "${INSTALL_CMAKE}" ] ; then
-    sudo apt update
-    sudo apt install cmake
-fi
-
-if [ "TRUE" == "${INSTALL_CPP_COMPILER}" ] ; then
-    sudo apt update
-    sudo apt install g++
-fi
-
-if [ "TRUE" == "$INSTALL_SDL" ] ; then
-    sudo apt install libx11-dev libxext-dev
-
-    wget https://github.com/libsdl-org/SDL/archive/refs/tags/release-2.24.0.zip
-    unzip release-2.24.0.zip
-    pushd SDL-release-2.24.0
-    ./configure
-    make
-    sudo make install
-    popd
-fi
-
-if [ "TRUE" == "$INSTALL_SDL_IMAGE" ] ; then
-    sudo apt install libx11-dev libxext-dev
-
-    wget https://github.com/libsdl-org/SDL_image/archive/refs/tags/release-2.6.2.zip
-    unzip release-2.6.2.zip
-    pushd SDL_image-release-2.6.2
-    ./configure
-    make
-    sudo make install
-    popd
-fi
-
-popd # deps_install_workarea
-
-pushd ../
-if [ ! -d external ] ; then
-    mkdir external
-fi
-
-popd # ../
-
-pushd ./deps_install_workarea
-if [ "TRUE" == "${INSTALL_GTEST}" ] ; then
-    ../install_gtest.sh
-fi
-popd # ./deps_install_workarea
-
-sudo rm -rf ./deps_install_workarea
+main
