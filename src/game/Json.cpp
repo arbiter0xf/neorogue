24661aa6791ca8066a0105a956daa592b1a98889
Aleksi Backman <arbiter0xf@protonmail.com>
2024-07-14 22:44:26 +0300

    Parse JSON with cJSON from file

--- src/game/Json.cpp
+++ src/game/Json.cpp
@@ -12,24 +12,88 @@
 
 cJSON* Json::readFromFile(std::string file_path)
 {
+    const int bytes = 1;
+    const char* cjson_error_ptr;
+
     int ret;
+    char* json_string;
+    std::string msg;
+    FILE* file;
+    cJSON* json;
     struct stat file_stat;
 
     ret = 0;
+    json = 0;
     file_stat = {};
+    json_string = 0;
 
     ret = stat(file_path.c_str(), &file_stat);
     if (0 != ret) {
-        std::string msg = "Failed to stat ";
+        msg = "Failed to stat ";
         msg += file_path;
         msg += " : ";
         msg += strerror(errno);
         throw std::runtime_error(msg);
     }
 
+    json_string = (char*) calloc(file_stat.st_size, bytes);
     printf("%s size is: %d\n", file_path.c_str(), file_stat.st_size);
 
-    return nullptr; // TODO return cJSON
+    file = fopen(file_path.c_str(), "r");
+    if (0 == file) {
+        msg = "Failed to open file ";
+        msg += file_path;
+        msg += " : ";
+        msg += strerror(errno);
+        throw std::runtime_error(msg);
+    }
+
+    ret = fread(json_string, bytes, file_stat.st_size, file);
+    if (ret != file_stat.st_size) {
+        free(json_string);
+        json_string = 0;
+
+        msg = "Read unexpected amount (";
+        msg += ret;
+        msg += ") of bytes from file ";
+        msg += file_path;
+        throw std::runtime_error(msg);
+    }
+
+    ret = fclose(file);
+    if (0 != ret) {
+        free(json_string);
+        json_string = 0;
+
+        msg = "Failed to close file ";
+        msg += file_path;
+        msg += " : ";
+        msg += strerror(errno);
+        throw std::runtime_error(msg);
+    }
+    file = 0;
+
+    json = cJSON_Parse(json_string);
+    if (nullptr == json) {
+        free(json_string);
+        json_string = 0;
+
+        cjson_error_ptr = cJSON_GetErrorPtr();
+
+        msg = "Failed to cJSON_Parse";
+        if (nullptr == cjson_error_ptr) {
+            throw std::runtime_error(msg);
+        }
+
+        msg += ": ";
+        msg += cjson_error_ptr;
+        throw std::runtime_error(msg);
+    }
+
+    free(json_string);
+    json_string = 0;
+
+    return json;
 }
 
 #if 0
